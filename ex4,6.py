5fb80ee402c32b1a9fe2ce801390cffb7370c81c
BALASUBRAMANIAN2511 <80732636+BALASUBRAMANIAN2511@users.noreply.github.com>
2021-03-16 08:22:12 +0530

    Can do changes

---
+++ ex4,6.py
@@ -0,0 +1,13 @@
+def computepay():
+    hrs=input()
+    h=float(hrs)
+    rate=input()
+    r=float(rate)
+    if h<=40:
+        pay=h*r
+    elif h>40:
+         d=h-40
+    pay=(40*r)+(d*(r*1.5))
+    print('Pay',pay)
+    return pay
+computepay()
